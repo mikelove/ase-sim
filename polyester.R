339d21d1c4ae4d273e5a99fcc5740f2101a5e8bc
mikelove <michaelisaiahlove@gmail.com>
2020-11-30 21:07:39 -0500

    more work

--- polyester.R
+++ polyester.R
@@ -1,3 +1,26 @@
+library(AnnotationHub)
+ah <- AnnotationHub()
+
+q <- query(ah, c("EnsDb",101,"Drosophila"))
+edb <- q[[1]] # Ensembl database 
+
+q <- query(ah, c("BDGP6.28",101,"TwoBit","dna_rm"))
+genome <- q[[1]] # genome file
+
+dna <- import(genome)[1:7]
+# flip a letter:
+# replaceLetterAt(dna, at, letter=as.character(complement(subseq(dna, at, at))))
+rtracklayer::export(dna, con="~/Desktop/test.2bit")
+genome_alt <- TwoBitFile("~/Desktop/test.2bit")
+
+ebt <- exonsBy(edb, by="tx")
+chroms <- c("2L","2R","3L","3R","4","X","Y")
+ebt <- keepSeqlevels(ebt, value=chroms, pruning.mode = "coarse")
+
+library(GenomicFeatures)
+cdna <- extractTranscriptSeqs(genome, ebt)
+cdna <- extractTranscriptSeqs(genome_alt, ebt)
+
 library(polyester)
 simulate_experiment(fasta=fasta, outdir=".",
                     num_reps=c(1,1),
