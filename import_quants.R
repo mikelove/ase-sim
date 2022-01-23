354612aeda19b76467c9c5b4dfa4e688b0931fe1
mikelove <michaelisaiahlove@gmail.com>
2022-01-23 14:31:37 -0500

    add to se coldata

--- import_quants.R
+++ import_quants.R
@@ -5,8 +5,10 @@ library(fishpond)
 library(SummarizedExperiment)
 samps <- list.files("quants")
 stopifnot(length(samps) == nsamp) # check that we have same number
+sample_name <- paste0("samp",seq_along(samps))
 coldata <- data.frame(files=file.path("quants",samps,"quant.sf"),
-                      names=samps)
+                      names=samps,
+                      sample=factor(sample_name))
 # txp-level data
 se <- importAllelicCounts(coldata, a1="P", a2="M", format="wide")                      
 save(se, file="txp_allelic_se.rda")
